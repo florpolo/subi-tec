a9c6d4c60e221ca0d4b40c63364fbff02e1668be
florpolo <110501764+florpolo@users.noreply.github.com>
2025-11-18 17:23:22 -0300

    Updated RemitoGenerator.ts

--- src/lib/RemitoGenerator.ts
+++ src/lib/RemitoGenerator.ts
@@ -17,8 +17,8 @@ const ANCHORS_PT = {
   DOMICILIO: { x: 150, y: 659.0, maxW: 430 },
 
   // Párrafo "Certifico que…"
-// -20 pt a la izquierda
-DESC_START: { x: 114.9, y: 560.9, maxW: 511.25, lineH: 16.5, maxLines: 10 },
+
+DESC_START: { x: 74.9, y: 560.9, maxW: 551.25, lineH: 16.5, maxLines: 10 }
 
   // N°
   NUMERO: { x: 379.0, y: 765.5, maxW: 112.5 },
